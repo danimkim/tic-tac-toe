eae0dabea59774bcbf1defd1eb310719e41f6288
danimkim <danimkim.dev@gmail.com>
2024-11-22 00:56:37 +0900

    feat: implement tic tac toe game logic

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './GameBoard.css';
 
 const initialGameBoard = [
@@ -7,14 +8,27 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIdx, colIdx, playerSymbol) => {
+    const updatedGameBoard = [...gameBoard.map((row) => [...row])];
+    updatedGameBoard[rowIdx][colIdx] = playerSymbol;
+    setGameBoard(updatedGameBoard);
+  };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIdx) => (
+      {gameBoard.map((row, rowIdx) => (
         <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() =>
+                    handleSelectSquare(rowIdx, colIdx, playerSymbol)
+                  }
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
